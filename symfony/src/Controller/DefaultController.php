e00955f1666e074c872734ecfc3e6405a4d41120
Aleksander Krawczyk <sandrotig@gmail.com>
2021-04-29 00:23:01 +0200

    init commit

---
+++ symfony/src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * Root Endpoint
+     * @Route("/",name="homepage")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'version' => '1.0',
+            'name' => 'AK_SYMFONY_API',
+        ]);
+    }
+}
