e00cf194c242e88a395ba005f95a43506ad9d0d7
Aleksander Krawczyk <sandrotig@gmail.com>
2021-05-01 15:12:54 +0200

    users crud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,29 +5,34 @@ namespace App\Controller;
 use App\Entity\Users;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\DBAL\Types\DateTimeType;
 use Exception;
-
+use JsonException;
 
 class UserController extends AbstractController
 {
-    
+
+
+
     /**
      * User List
      * @Route("/users",name="users_list",methods={"GET"})
      * 
      */
 
-    public function ListUsers(){
+    public function ListUsers()
+    {
 
-        
-        $users=$this->getDoctrine()->getManager()->getRepository(Users::class)->findAll();
-        
-        return $this->json($list,200);
-        
+
+        $users = $this->getDoctrine()->getManager()->getRepository(Users::class)->findAll();
+
+        $list = [];
+        foreach ($users as $user) {
+            $list[] = array("id" => $user->getId(), "name" => $user->getName(), "email" => $user->getEmail(), "password" => $user->getPassword(),"createdate"=>$user->getCreateDate());
+        }
+
+        return $this->json($list, 200);
     }
 
     /**
@@ -36,19 +41,21 @@ class UserController extends AbstractController
      * 
      */
 
-    public function AddUsers(Request $request){
-        $dane=json_decode($request->getContent(),true);
-        $user=new Users();
+    public function AddUsers(Request $request)
+    {
+        $dane = json_decode($request->getContent(), true);
+
+        $user = new Users();
         $user->setName($dane['name']);
         $user->setEmail($dane['email']);
         $user->setPassword("Blank");
         $user->setCreatedate(new DateTime());
-        
+
         $this->getDoctrine()->getManager()->persist($user);
         $this->getDoctrine()->getManager()->flush();
-        
 
-        return $this->json($dane,201);
+
+        return $this->json($dane, 201);
     }
 
     /**
@@ -57,15 +64,16 @@ class UserController extends AbstractController
      * 
      */
 
-    public function DeleteUsers($id){
-        
-        $user=$this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
-        if(!$user){
-            throw new Exception("No data found");
+    public function DeleteUsers($id)
+    {
+
+        $user = $this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException("No data found");
         }
         $this->getDoctrine()->getManager()->remove($user);
         $this->getDoctrine()->getManager()->flush();
-        return $this->json(['users'=>$id],200);
+        return $this->json(['users' => $id], 200);
     }
 
     /**
@@ -74,28 +82,23 @@ class UserController extends AbstractController
      * 
      */
 
-    public function UpdateUsers($id,Request $request){
+    public function UpdateUsers($id, Request $request)
+    {
 
-        $dane=json_decode($request->getContent(),true);
-        $user=$this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
-        $user=$this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
-        if(!$user){
-            throw new Exception("No data found");
+        $dane = json_decode($request->getContent(), true);
+        $user = $this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
+        $user = $this->getDoctrine()->getManager()->getRepository(Users::class)->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException("No data found");
         }
         $user->setName($dane['name']);
         $user->setEmail($dane['email']);
         $user->setPassword("Blank");
-        //$user->setCreatedate(new DateTime());
-        
+
         $this->getDoctrine()->getManager()->persist($user);
         $this->getDoctrine()->getManager()->flush();
-        
 
-        return $this->json($dane,201);
+
+        return $this->json($dane, 200);
     }
-
-
 }
-
-
-?>
