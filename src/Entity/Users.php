e74b42903fe7633af7bfbfffac7d8f676b02e152
Aleksander Krawczyk <sandrotig@gmail.com>
2021-05-11 23:58:12 +0200

    entity refactor

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,17 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-
-
-
-
-
 
 /**
- * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @ORM\Entity()
+ * @ORM\Table(name="Users")
  */
 class Users
 {
