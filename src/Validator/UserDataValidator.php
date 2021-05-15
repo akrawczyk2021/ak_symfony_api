36523f91a06eab1b6573c3dd04ea22f977b56065
Aleksander Krawczyk <sandrotig@gmail.com>
2021-05-16 00:30:49 +0200

    email validation

--- src/Validator/UserDataValidator.php
+++ src/Validator/UserDataValidator.php
@@ -7,31 +7,22 @@ namespace App\Validator;
 class UserDataValidator
 {
     const PATTERN = '/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/';
+    const EMAILPATTERN = '/(?=.*[a-zA-Z])(?=.*[\.])(?=.*[\@])/';
 
     public function isValidName(string $name): bool
     {
-        if (empty($name)) {
-            return false;
-        } else {
-            return true;
-        }
+        return !empty($name);
     }
 
     public function isValidEmail(string $email): bool
     {
-        if (empty($email)) {
-            return false;
-        } else {
-            return true;
-        }
+        return !empty($email) && preg_match(self::EMAILPATTERN, $email) === 1;
     }
 
     public function isValidPassword(string $password): bool
     {
-        if (empty($password) || preg_match(self::PATTERN, $password) == false) {
-            return false;
-        } else {
-            return true;
-        }
+        return !empty($password) && preg_match(self::PATTERN, $password) === 1;
     }
+
+
 }
