7bfa949abfc9ad88d07fad835cf68fe009cc751b
Aleksander Krawczyk <sandrotig@gmail.com>
2021-06-09 23:45:31 +0200

    Card Entity

---
+++ src/Entity/Card.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="Card")
+ */
+class Card
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $att;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $deff;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAtt(): ?int
+    {
+        return $this->att;
+    }
+
+    public function setAtt(int $att): self
+    {
+        $this->att = $att;
+
+        return $this;
+    }
+
+    public function getHp(): ?int
+    {
+        return $this->hp;
+    }
+
+    public function setHp(int $hp): self
+    {
+        $this->hp = $hp;
+
+        return $this;
+    }
+
+    public function getDeff(): ?int
+    {
+        return $this->deff;
+    }
+
+    public function setDeff(int $deff): self
+    {
+        $this->deff = $deff;
+
+        return $this;
+    }
+}
