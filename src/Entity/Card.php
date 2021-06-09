5f48c09f18dc16045e5aa6357a9d6c76f4087b05
akrawczyk2021 <83376556+akrawczyk2021@users.noreply.github.com>
2021-06-10 00:57:29 +0200

    Ft task#10 create card entity (#6)

---
+++ src/Entity/Card.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="Card")
+ */
+class Card
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string",length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $attack;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $defence;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAttack(): ?int
+    {
+        return $this->attack;
+    }
+
+    public function setAttack(int $attack): self
+    {
+        $this->attack = $attack;
+
+        return $this;
+    }
+
+    public function getHp(): ?int
+    {
+        return $this->hp;
+    }
+
+    public function setHp(int $hp): self
+    {
+        $this->hp = $hp;
+
+        return $this;
+    }
+
+    public function getDefence(): ?int
+    {
+        return $this->deffence;
+    }
+
+    public function setDefence(int $defence): self
+    {
+        $this->defence = $defence;
+
+        return $this;
+    }
+}
